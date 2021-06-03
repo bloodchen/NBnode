e5c62ce69cbd8a1c623d88886f0ea1776e306834
bloodchen <bloodchen@gmail.com>
2021-06-03 16:23:26 +0800

    update

--- modules/api/index.js
+++ modules/api/index.js
@@ -19,8 +19,8 @@ const { request } = require('express');
 var app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
 const auth = async (req, res) => {
     try {
